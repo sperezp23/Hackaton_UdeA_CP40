89c1c549a7e1a6a587826789eaac889528ec84a9
andresqb198 <andresqb198@gmail.com>
2023-09-29 14:26:50 -0500

    Prompt functionality ready

--- Front/src/components/Header/Header.jsx
+++ Front/src/components/Header/Header.jsx
@@ -7,8 +7,7 @@ const Header = () => {
         <img src="../public/Logo.png" alt="LLMed" />
       </figure>
       <h1 className="header__title">
-        Transformando Datos en Diagnósticos: Tu Socio en la Lucha contra la
-        Desinformación.
+        Transformando Datos en Diagnósticos
       </h1>
     </div>
   );
